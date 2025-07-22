a0cdd944e39453d13f65abf3ee621481478997b0
uniqueahmerd <ahmerdothman@gmail.com>
2025-07-22 19:04:23 +0100

    update the cookie config

--- utils/generateTokenCookie.js
+++ utils/generateTokenCookie.js
@@ -5,7 +5,7 @@ const generateToken = (id, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
   return token;
